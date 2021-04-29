e15e1adf405c15768082ecbe37a0c9bb303e6824
roal <roaldys@gmail.com>
2021-04-28 20:31:34 -0500

    Agregado todos los modelos de base dato

--- app/user/models.py
+++ app/user/models.py
@@ -3,42 +3,42 @@ from django.db import models
 # Create your models here.
 
 from django.contrib.auth.models import AbstractUser
+from core.preMatricula.models import Municipio
 from config.settings import MEDIA_URL, STATIC_URL
 
-CHOICES_CARGO = [
-    ("c", "Instructor C"),
-    ("d", "Instructor D"),
-    ("e", "Económico"),
-    ("d", "Director")
-
-]
-
-
+class Estado(models.Model):
+    nombre = models.CharField(max_length=15,verbose_name="Nombre Estado",unique=True)
+    def __str__(self):
+        return self.nombre
 class User(AbstractUser):
-    cargo = models.CharField(
-        max_length=15, choices=CHOICES_CARGO, null=True, blank=True)
-
+    estado= models.ForeignKey(Estado,on_delete=models.SET_NULL,verbose_name="Estado del Usuario",null=True,blank=True)
     def __str__(self):
         return f"{self.username}"
 
 class Perfil(models.Model):
+    
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, verbose_name="Usuario")
-    avatar = models.ImageField(
-        upload_to='users/avatar', verbose_name="Avatar")
-    estudios = models.CharField(
-        max_length=200, null=True, blank=True, verbose_name="Estudios")
+    nombre = models.CharField(max_length=150, verbose_name="Nombre")
+    apellido1 = models.CharField(
+        max_length=150, verbose_name="Apellido Paterno")
+    apellido2 = models.CharField(
+        max_length=150, verbose_name="Apellido Materno")
     direccion = models.CharField(
         max_length=200, null=True, blank=True, verbose_name="Dirección")
+    municipio = models.ForeignKey(Municipio,on_delete=models.RESTRICT,verbose_name='Municipio')
     telefono = models.CharField(
         max_length=15, null=True, blank=True, verbose_name="Teléfono")
-    notas = models.CharField(max_length=200, null=True,
-                             blank=True, verbose_name="Notas")
-
+    correo = models.CharField(max_length=100,verbose_name="Correo")
+    avatar = models.ImageField(
+        upload_to='users/avatar', verbose_name="Avatar",null=True,blank=True)
+        
     def __str__(self):
-        return f"{self.user.username}"
+        return f"{self.nombre}-{self.user.username}"
 
     def get_image(self):
+        print("Hollaalalalalala desde avatar")
         if self.avatar:
             return '{}{}'.format(MEDIA_URL, self.avatar)
-        return '{}{}'.format(STATIC_URL, 'img/empty.png')
+        print('{}{}'.format(STATIC_URL, 'img/default.png'))
+        return '{}{}'.format(STATIC_URL, 'img/default.png')
