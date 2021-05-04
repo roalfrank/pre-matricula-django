d5eeeb3bc31917b72a52c7e6cd4ae46c706df702
roal <roaldys@gmail.com>
2021-05-04 06:29:20 -0500

    ya tiene implementado el delete

--- app/core/preMatricula/logica/tbentidad/Provincia/views.py
+++ app/core/preMatricula/logica/tbentidad/Provincia/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.db.models import  Q
 import time
+from django.db.models.deletion import RestrictedError
 
 
 class ProvinciaListView(TemplateView):
@@ -22,6 +23,7 @@ class ProvinciaListView(TemplateView):
         data = {}
         try:
             action = request.POST['action']
+            #action addd para adicionar registro
             if action == 'add':
                 form = ProvinciaForm(request.POST)
                 if form.is_valid():
@@ -31,36 +33,35 @@ class ProvinciaListView(TemplateView):
                 else:    
                     data['enviado']= False
                     data['error']="Error insertando dato"
+            #action chequearProvincia , esto es para comprobar rapido si ya hay otra provincia con el Mismo Nombre
             elif action == 'chequearProvincia':
                 try:
-                    
+                    #chequeamos si estamos en editar o add . si es editar hacemos la consulta con NONe
+                    #edit_id no se incluye en la consulta 
                     id_edit = request.POST['edit_id']
                     if id_edit == "":
                         id_edit = None
-
+                    #consulta a la base dato chequear el nombre de la provincia.
                     provincia = Provincia.objects.filter(
                         ~Q(id=id_edit), nombre__iexact=request.POST['nombre'])
-                    print(provincia)
                     if len(provincia) < 1:
+                        #si no hay resultado en la busqueda , se devuelve True, dando lus verde a esa provincia.
                         return JsonResponse(True, safe=False)
+                    #si hay resultado se retorna Fasle para impedir que se cree la provincia con nombres repetidos.
                     return JsonResponse(False,safe=False)
                 except:
                     return JsonResponse(False,safe=False)
+            # Action  cargarDatos - para cargar la datatable de mi vista.
             elif action == "cargarDatos":
-                # for n in range(31, 300):
-                #     nombre = f"roal-{n}"
-                #     provincia = Provincia(nombre=nombre)
-                #     provincia.save()
-                tiempo_inicial = time.time()
+                #tiempo_inicial = time.time()
                 data = [i.toJson()  for i in Provincia.objects.all()]
                 respuesta = JsonResponse(data, safe=False)
-                tiempo = time.time() - tiempo_inicial
-                print(tiempo)
+                #tiempo = time.time() - tiempo_inicial
+                #print(tiempo)
                 return respuesta
+            #action edit - editando una provincia.
             elif action=='edit':
-                print(request.POST)
                 provincia=Provincia.objects.get(pk=int(request.POST['id_edit']))
-                print(provincia)
                 form = ProvinciaForm(request.POST,instance=provincia)
                 if form.is_valid():
                     form.save()
@@ -69,11 +70,22 @@ class ProvinciaListView(TemplateView):
                 else:
                     data['enviado']= False
                     data['error'] = "Error editando datos"
+            #action delete, eliminar una provincia
+            elif action=="delete":
+                try:
+                    delet_provincia = Provincia.objects.get(pk=int(request.POST["id"]))
+                    delet_provincia.delete()
+                except Exception as e:
+                    if type(e) == RestrictedError:
+                        data['error'] = "Esta provincia tiene municipios, no se puede borrar."
+                    else:
+                        data['error']="Error durante la eliminacion del registro."
         except Exception as e:
+            #comprobar los errores , esto despues solo dejar a data['error']
             print(e)
             print(e.args)
             print(type(e))
-            data['error'] = 'Ha ocurrido un error '
+            data['error'] = 'Error en las operaciones con los registros'
         return JsonResponse(data)
             
     def get_context_data(self, **kwargs):
--- app/static/js/funcion.js
+++ app/static/js/funcion.js
@@ -55,3 +55,46 @@ function submit_with_ajax(url, title, content, parameters, callback) {
         }
     })
 }
+function with_ajax(url, title, content, parameters, callback) {
+    $.confirm({
+        theme: 'material',
+        title: title,
+        icon: 'fa fa-info',
+        content: content,
+        columnClass: 'small',
+        typeAnimated: true,
+        cancelButtonClass: 'btn-primary',
+        draggable: true,
+        dragWindowBorder: false,
+        buttons: {
+            info: {
+                text: "Si",
+                btnClass: 'btn-primary',
+                action: function () {
+                    $.ajax({
+                        url: url, //window.location.pathname
+                        type: 'POST',
+                        data: parameters,
+                        dataType: 'json'
+                    }).done(function (data) {
+                       
+                        callback(data);
+                       
+                     
+                    }).fail(function (jqXHR, textStatus, errorThrown) {
+                        alert(textStatus + ': ' + errorThrown);
+                    }).always(function (data) {
+
+                    });
+                }
+            },
+            danger: {
+                text: "No",
+                btnClass: 'btn-red',
+                action: function () {
+
+                }
+            },
+        }
+    })
+}
