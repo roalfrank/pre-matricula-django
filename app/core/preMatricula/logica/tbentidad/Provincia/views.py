732285c0aae7e7343ddb5b08822fcaf1dd4be12c
roal <roaldys@gmail.com>
2021-05-04 03:12:37 -0500

    Funcion listar, crear y editar de Provincia

--- app/core/preMatricula/logica/tbentidad/Provincia/views.py
+++ app/core/preMatricula/logica/tbentidad/Provincia/views.py
@@ -6,6 +6,8 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.db.models import  Q
+import time
 
 
 class ProvinciaListView(TemplateView):
@@ -17,25 +19,27 @@ class ProvinciaListView(TemplateView):
         return super().dispatch(request, *args, **kwargs)
     
     def post(self,request,*args,**kwargs):
-        #print('Hola estoy en crear provincia principio')
         data = {}
         try:
             action = request.POST['action']
-            print(request.POST)
             if action == 'add':
-                print('Hola estoy en crear provincia dentro')
                 form = ProvinciaForm(request.POST)
-                print(form)
                 if form.is_valid():
                     form.save()
                     data['enviado']=True
                     data['nombre'] = request.POST['nombre']
                 else:    
                     data['enviado']= False
+                    data['error']="Error insertando dato"
             elif action == 'chequearProvincia':
-                #print(request.POST['nombre'][0])
                 try:
-                    provincia = Provincia.objects.filter(nombre__iexact=request.POST['nombre'])
+                    
+                    id_edit = request.POST['edit_id']
+                    if id_edit == "":
+                        id_edit = None
+
+                    provincia = Provincia.objects.filter(
+                        ~Q(id=id_edit), nombre__iexact=request.POST['nombre'])
                     print(provincia)
                     if len(provincia) < 1:
                         return JsonResponse(True, safe=False)
@@ -43,11 +47,32 @@ class ProvinciaListView(TemplateView):
                 except:
                     return JsonResponse(False,safe=False)
             elif action == "cargarDatos":
+                # for n in range(31, 300):
+                #     nombre = f"roal-{n}"
+                #     provincia = Provincia(nombre=nombre)
+                #     provincia.save()
+                tiempo_inicial = time.time()
                 data = [i.toJson()  for i in Provincia.objects.all()]
-                print(data)
-                return JsonResponse(data,safe=False)
+                respuesta = JsonResponse(data, safe=False)
+                tiempo = time.time() - tiempo_inicial
+                print(tiempo)
+                return respuesta
+            elif action=='edit':
+                print(request.POST)
+                provincia=Provincia.objects.get(pk=int(request.POST['id_edit']))
+                print(provincia)
+                form = ProvinciaForm(request.POST,instance=provincia)
+                if form.is_valid():
+                    form.save()
+                    data['enviado'] = True
+                    data['nombre'] = request.POST['nombre']
+                else:
+                    data['enviado']= False
+                    data['error'] = "Error editando datos"
         except Exception as e:
             print(e)
+            print(e.args)
+            print(type(e))
             data['error'] = 'Ha ocurrido un error '
         return JsonResponse(data)
             
